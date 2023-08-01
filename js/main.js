72b840f50dee2772fd04885404c441653091c5d6
BANG <82694273+Bangsuhyun96@users.noreply.github.com>
2023-08-01 15:08:48 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
-
-    const array = new Array(10); // 배열 선언
-    let timecnt = 3;
-    let gametime = 30;
-    time();
-    startgame();
-    //startgame(); function은 자동 실행되지 않음
-    let score=0;
-    let combo=0;
-    const left = document.querySelector('.classcomp .standard0'); //왼쪽 버튼
-    const right = document.querySelector('.classcomp .standard1'); // 오른쪽 버튼
+    // const left = document.querySelector('.classcomp .standard0'); //왼쪽 버튼
+    // const right = document.querySelector('.classcomp .standard1'); // 오른쪽 버튼
 
     // APK 버전
     // left.addEventListener('click',(e)=>{
@@ -66,12 +57,60 @@
     //     });       
     // }, 3000);
 
-    // PC버전    
+    const array = new Array(10);
+    let timecnt = 3;
+    let gametime = 30;
+    let feverSwitch = 0; // 현재 상태
+    let feverCount = 0; // 피버 진입 조건
+    time();
+    startgame();
+    let score=0;
+    let combo=0;
+
+    // PC버전
     function game(){
+        if(feverSwitch==0){
+            normalEvent(); // 일반상태 진입
+        }else if(feverSwitch==1){
+            feverEvent(); // 피버상태 진입
+        }
+    }
+    
+    function feverOn(){
+        let feverClassLoc = document.querySelectorAll('.createloc > div');
+        for(let i = 0; i <= feverClassLoc.length-1; i++){
+            feverClassLoc[i].classList.add('fever');
+        }
+    }
+
+    function feverOff(){
+        feverSwitch=0;
+        let feverClassLoc = document.querySelectorAll('.createloc > div');
+        for(let i = 0; i <= feverClassLoc.length-1; i++){
+            feverClassLoc[i].classList.remove('fever');          
+        }
+        return;
+    }
+    
+    // 피버모드 진입
+    function feverEvent(){
+        gametime+=3; // 피버 진입 시 3초 추가
+        gametxt(); // 추가된 시간 출력
+        document.addEventListener('keydown', (e)=>{ // 좌우 입력
+            if(e.code === 'ArrowLeft' || e.code === 'ArrowRight'){
+                addnum(); // 배열 갱신
+                feverOn(); // 갱신된 객체 피버상태 적용
+            }            
+        });
+        feverOff(); // 피버 종료 (조건부 추가 → 종료가 안됨)
+        return;
+    }
+
+    // 일반모드 진입
+    function normalEvent(){
         setTimeout(() => {
             document.addEventListener('keydown',(e)=>{
                 const firstElem = document.querySelector('.createloc > div');
-                
                 if(firstElem.className === 'standard0'){
                     if(e.code === 'ArrowLeft'){
                         scorecheck();
@@ -92,10 +131,10 @@
         }, 3000);
     }
 
-    function startgame(){  // 게임 시작 시 생성되는 배열
+    function startgame(){
         for(let i=0; i<array.length; i++){
             let randomnum = Math.floor(Math.random()*2);
-            array[i] = randomnum; // 초기에는 push 사용 X;
+            array[i] = randomnum;
             const createloc = document.querySelector('.createloc');
             const div = document.createElement('div');
             div.className = 'standard'+array[i];
@@ -105,22 +144,13 @@
         return;
     }
 
-    /* 배열 임의의 위치에 요소 추가 제거 */
-    // array.splice(start,deleteCount,el);
-    // start - 수정할 배열 요소의 인덱스
-    // deleteCount - 삭제할 요소 개수, 제거하지 않을 경우 0
-    // el - 배열에 추가될 요소
-
-    function addnum(){ // 다음 숫자 추가 -> 클래스랑 연동 필요
+    function addnum(){
         let randomnum2 = Math.floor(Math.random()*2);
         array.splice(0,1);
         array.push(randomnum2);
-
         let findstandard0 = document.querySelector('.createloc div');
         let findstandard = document.querySelector('.createloc');
         findstandard.removeChild(findstandard0);
-        //findstandard.parentnodes.remove(findstandard0);
-
         const createloc = document.querySelector('.createloc');
         const div = document.createElement('div');
         div.className = 'standard'+randomnum2;
@@ -142,10 +172,14 @@
 
     function combocnt(){
         combo ++;
+        feverCount ++;
         if(combo>=2){
             document.querySelector('.combo').innerHTML = combo + " Combo !";
         }
         comboPlusanimate();
+        if(feverCount==40){
+            feverSwitch=1;
+        }
     }
 
     function comboreset(){
@@ -186,13 +220,13 @@
             {scale : 1.0, opacity: 1.0}
         ],400);
     }
+
     function comboPlusanimate(){
         document.querySelector('.combo').animate([
             {scale : 1.2,},
             {scale : 1.0,}
         ],300);
     }
-
 
     function time(){
         document.querySelector('.startcnt').innerHTML = timecnt;
@@ -201,15 +235,19 @@
         if(timecnt==-1){
             document.querySelector('.cloud').style.display='none';
             setTimeout(() =>{
-                clearInterval(timereset); // 카운트가 종료되고 더이상 돌지 않음
+                clearInterval(timereset);
             gametimer();
             },100);
         }
     }
 
-    function gametimer(){
+    function gametxt(){
         document.querySelector('.timerresult').innerHTML = gametime+"초";
+    }
+
+    function gametimer(){
         gametime--;
+        gametxt();
         let gametumercnt = setTimeout(gametimer, 1000);
         if(gametime<10){
             document.querySelector('.timer > p').style.color = "red";
